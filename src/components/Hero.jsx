0c27b613177b09a6c00245ed0fcbd15afc77faf4
VS <anaberrantis@gmail.com>
2023-04-28 14:30:23 +0200

    add components to hero secrtion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,18 +7,44 @@ const Section = styled.div`
   background: black;
   scroll-snap-align: center;
   flex-direction:column;
-  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
 `;
+
 const Container = styled.div`
   height: 100vh;
-`
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content: space-between;
+`;
 
+const TopLeft = styled.div``;
+const BotRight = styled.div``;
+const WhatIDo = styled.div``;
+const Title = styled.h1``;
+const Desc = styled.p``;
+const Line = styled.div``;
+const Subtitle = styled.h2``;
+const Button = styled.button``;
 
 const Hero = () => {
   return(
     <Section>
       <Navbar/>
-      <Container>Hero</Container>
+      <Container>
+        <TopLeft>
+          <Title>Ahoj. Paca. Mama.</Title>
+          <WhatIDo></WhatIDo>
+          <Desc>Mam rad pacu aj lejcu</Desc>
+            <Line/>
+            <Subtitle></Subtitle>
+        </TopLeft>
+        <BotRight>
+          {/* {3d model} */}
+          <img src="./img/test400x490.png" />
+        </BotRight>
+      </Container>
     </Section>
   );
 };
